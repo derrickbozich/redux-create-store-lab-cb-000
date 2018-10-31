af2d7540053790a3b631da152dd9553b091cf340
derrick b <dbozich1@gmail.com>
2018-10-31 22:22:41 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,3 @@
-import candyReducer from './reducers/candyReducer';
-import countReducer from './reducers/countReducer';
 
 export default function createStore(reducer) {
   // add your code here
@@ -24,4 +22,24 @@ countStore.dispatch({type: '@@INIT'})
 
 function render() {
   const container = document.getElementById('container');
+}
+
+function candyReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_CANDY':
+      return [...state, action.candy];
+    default:
+      return state;
+  }
+}
+
+function countReducer(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT_COUNT':
+      return state + 1;
+    case 'DECREMENT_COUNT':
+      return state - 1;
+    default:
+      return state;
+  }
 }
